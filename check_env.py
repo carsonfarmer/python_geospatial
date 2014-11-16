230cf79e69534d41a9eb8bb43d22e3eab47a50d2
Carson Farmer <carsonfarmer@gmail.com>
2014-11-16 17:56:29 -0500

    Update check_env.py
    Removed some old checks and updated warnings

--- check_env.py
+++ check_env.py
@@ -22,24 +22,14 @@ with warnings.catch_warnings(record=True) as w:
 
     # Optional stuff
     try:
-        import mpl_toolkits.basemap
         import psycopg2
+        import mpl_toolkits.basemap
     except ImportError, err:
         print("Error: " + unicode(err))
-        print("This is not a required package.\n")
-        print("See Known Issues for additional information: "
-              "https://github.com/cfarmer/python_geospatial/blob/master/install.md#known-issues")
-
-    if len(w) > 0:
-        for i in w:
-            if "openpyxl" in unicode(i.message):
-                print("\nPandas is installed, but you might have a dependency issue. "
-                      "To fix this, try the following:\n"
-                      "   $ pip install openpyxl\n"
-                      "   $ pip uninstall openpyxl\n"
-                      "   $ pip install openpyxl==1.8.6\n"
-                      "See Known Issues for additional information: "
-                      "https://github.com/cfarmer/python_geospatial/blob/master/install.md#known-issues")
+        print("This is not a required package. "
+              "See Known Issues for additional information:\n"
+              "https://github.com/carsonfarmer/python_geospatial/blob/master/"
+              "install.md#known-issues")
         import sys
         sys.exit(0)
 
